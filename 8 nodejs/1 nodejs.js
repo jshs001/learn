24681342987f304d10ab33d23bcaf1f6033a0bd2
cc <cc.com>
2026-05-24 20:00:00 +0800

    node

--- 8 nodejs/1 nodejs.js
+++
@@ -1,7 +0,0 @@
-// node.js 是基于Chrome V8引擎的JavaScript运行环境。
-// 浏览器是js的前端运行环境，node.js是js的后端运行环境。node.js中无法调用DOM和BOM等浏览器内置对象。
-
-// node.js仅仅提供了基础的功能和api。然而很多工具和框架如雨后春竹。如express（快速构建web应用）、electron（构建桌面应用）、restify（快速构建API接口项目）等
-// node.js安装过程略 注意安装LTS版本（长期稳定版）
-
-console.log("hello world");
